4d4e2637e91756c253398f66476fc08df6913778
Mattvand14 <mattvand@ad.unc.edu>
2024-05-23 18:00:28 -0400

    frontend setup

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+    return (
+        <div className='imageDiv'>
+            <img className='spotifyLogo' src='/images/spotifyLogo.png' alt='Spotify Logo' />
+        </div>
+    );
+}
+
+export default App;
